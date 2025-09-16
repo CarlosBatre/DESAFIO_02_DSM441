46e943792db53795f07cc69c4fe15860b211e843
Carlos Ruano <carlosbatres.gg@gmail.com>
2025-09-15 22:58:05 -0600

    proyecto + auth

---
+++ app/src/main/java/udb/edu/sv/ventaexpressmuebles/ClientesActivity.kt
@@ -0,0 +1,32 @@
+// ClientesActivity.kt
+package udb.edu.sv.ventaexpressmuebles
+
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.OnBackPressedCallback
+import androidx.appcompat.app.AppCompatActivity
+
+class ClientesActivity : AppCompatActivity() {
+
+    private lateinit var btnBack: Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_clientes)
+
+        btnBack = findViewById(R.id.btnBack)
+
+        // Configurar callback para el botón físico de atrás
+        val callback = object : OnBackPressedCallback(true) {
+            override fun handleOnBackPressed() {
+                finish()
+            }
+        }
+        onBackPressedDispatcher.addCallback(this, callback)
+
+        // Funcionalidad del botón de regresar
+        btnBack.setOnClickListener {
+            finish()
+        }
+    }
+}
